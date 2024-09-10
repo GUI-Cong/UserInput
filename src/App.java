e0ace49973cf320699e88bbb3d8df159ef9bb0ec
GUI-Cong <cong.gui@student.hamk.fi>
2024-09-10 15:26:25 +0800

    change to if condition

--- src/App.java
+++ src/App.java
@@ -10,7 +10,12 @@ public class App {
         int number2 = num.nextInt();
         int sum = number1 + number2;
         
-        System.out.println("The sum is " + sum);
+        if(sum > 10){
+            System.out.println("The value is greater than 10. It is " + sum + ".");
+        }
+        else{
+            System.out.println("The sum is " + sum);
+        }
 
 
     }
